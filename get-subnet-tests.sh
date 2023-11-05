4441bece7daf2efc3b36259457f43dd422049ccd
blunderful-scripts <134004289+blunderful-scripts@users.noreply.github.com>
2023-11-05 02:02:30 +0200

    Add files via upload

---
+++ get-subnet-tests.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+
+# tests for expand_ipv6 and compress_ipv6
+
+#### Initial setup
+
+me=$(basename "$0")
+
+script_dir=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)
+# shellcheck disable=SC2015
+[ -n "$script_dir" ] && cd "$script_dir" || { echo "$me: Error: Couldn't cd into '$script_dir'." >&2; exit 1; }
+
+export test_cut_ip="true"
+. "$script_dir/cut-ip.sh" || { echo "$me: Error: Can't source '$script_dir/cut-ip.sh'." >&2; exit 1; }
+
+# unit tests for compress_ipv6
+test_exp_comp_ipv6(){
+
+tests=" \
+2001:4567:1212:00b2:0000:0000:0000:0000 2001:4567:1212:b2::
+2001:4567:1111:56ff:0000:0000:0000:0000 2001:4567:1111:56ff::
+2001:4567:1111:56ff:0000:0000:0000:0001 2001:4567:1111:56ff::1
+ff02:0000:0000:0000:0000:0000:0000:0001 ff02::1
+ff05:0000:0000:0000:0000:0000:0001:0003 ff05::1:3
+ff02:0000:0000:0000:0000:0001:ff00:0001 ff02::1:ff00:1
+ff02:0000:0000:0000:0000:0001:ffce:4ee3 ff02::1:ffce:4ee3
+fe80:0000:0000:0000:021b:2bff:fece:4ee3 fe80::21b:2bff:fece:4ee3
+fd0e:2146:5cf5:4560:0000:0000:0000:0001 fd0e:2146:5cf5:4560::1
+2001:0db8:0000:0000:0000:0000:0002:0001 2001:db8::2:1
+2001:0db8:0000:0001:0001:0001:0001:0001 2001:db8:0:1:1:1:1:1
+2001:0db8:0000:0000:0001:0000:0000:0001 2001:db8::1:0:0:1
+2001:0db8:0000:0000:0000:0000:0000:0001 2001:db8::1
+0000:0000:0000:0000:0000:0000:0000:0000 ::
+0000:0000:0000:0000:0000:0000:0000:0001 ::1
+0001:0000:0000:0000:0000:0000:0000:0000 1::
+"
+
+	# remove extra spaces and tabs
+	tests="$(printf "%s" "$tests" | awk '{$1=$1};1')"
+
+	tests_num="$(echo "$tests" | wc -l)"
+
+	status_comp=0; status_exp=0
+	tests_done=0
+
+	for i in $(seq 1 "$tests_num" ); do
+		line="$(echo "$tests" | awk -v i="$i" 'NR==i{ print; }')"
+		expanded_ip="$(printf "%s" "$line" | cut -d' ' -f1 )"
+		compressed_ip="$(printf "%s" "$line" | cut -d' ' -f2 )"
+		if [ -n "$expanded_ip" ] && [ -n "$compressed_ip" ]; then
+			printf "%s" "."
+
+			# compress_ipv6 test
+			result="$(compress_ipv6 "$expanded_ip")"
+			if [ "$result" != "$compressed_ip" ]; then
+				echo "compress_ipv6 '$expanded_ip': expected '$compressed_ip', got '$result'."
+				status_comp=1
+			fi
+
+			# this tests the validate_ip() function
+			validate_ip "$result" "inet6" || echo "result failed validation: '$result'" >&2
+
+
+			# expand_ipv6 test
+			result="$(expand_ipv6 "$compressed_ip")"
+			if [ "$result" != "$expanded_ip" ]; then
+				echo "compress_ipv6 '$compressed_ip': expected '$expanded_ip', got '$result'."
+				status_exp=1
+			fi
+
+			# this tests the validate_ip() function
+			validate_ip "$result" "inet6" || echo "result failed validation: '$result'" >&2
+
+
+			tests_done=$((tests_done+1))
+		fi
+	done
+	return $((status_exp + status_comp))
+}
+
+test_exp_comp_ipv6
+echo
+echo "Tests done: $tests_done"
+echo "Test status: $?"
---
+++ get-subnet.sh
@@ -0,0 +1,255 @@
+#!/bin/sh
+
+# get-subnet.sh
+
+# cuts an ip to given length, outputs subnet with mask bits
+
+# requires ip with mask bits in 1st arg, ip family (inet or inet6) in 2nd arg
+
+# this is a modified and improved version of a script found here:
+# https://github.com/chmduquesne/wg-ip/blob/master/wg-ip
+# also used some input from here:
+# https://stackoverflow.com/questions/14697403/expand-ipv6-address-in-shell-script
+
+# the script should be POSIX-compatible
+# requires awk, grep with ERE support, sed and some additional standard utilities like tr and cut.
+# only tested with GNU variants, and only on Linux.
+
+
+# convert hex to dec (portable version)
+hex2dec() (
+	hex="$*"
+	for i in $hex; do
+		printf "%d " "$(( 0x$i ))"
+	done
+)
+
+# converts given ip address into 16 1-byte chunks
+ip_to_bytes() (
+	ip="$1"
+	family="$2"
+
+	case "$family" in
+		inet )	printf "%s" "$ip" | tr '.' ' ' ;;
+		inet6 )
+			expanded_ip="$(expand_ipv6 "$ip")"
+			validate_ip "$expanded_ip" "$family" || \
+				{ echo "ip_to_bytes(): Failed to expand ip '$ip'. Resulting address '$expanded_ip' is invalid."; return 1; }
+			split_exp_ip="$(printf "%s" "$expanded_ip" | tr -d ':' | sed 's/.\{2\}/& /g')"
+			hex2dec "$split_exp_ip" ;;
+		* ) echo "ip_to_bytes(): Error: invalid family '$family'" >&2; return 1 ;;
+	esac
+)
+
+# expands given ipv6 address
+expand_ipv6() (
+	addr="$1"
+	# prepend 0 if we start with :
+	printf "%s" "$addr" | grep -qs "^:" && addr="0${addr}"
+
+	# expand ::
+	if printf "%s" "$addr" | grep -qs "::"; then
+		colons=$(printf "%s" "$addr" | sed 's/[^:]//g')
+		missing=$(printf "%s" ":::::::::" | sed "s/$colons//")
+		expanded=$(printf "%s" "$missing" | sed 's/:/:0/g')
+		addr=$(printf "%s" "$addr" | sed "s/::/$expanded/")
+	fi
+	blocks=$(printf "%s" "$addr" | tr ':' ' ')
+	blocks="$(hex2dec "$blocks")"
+	for block in $blocks; do
+		blocks_temp="$blocks_temp$(printf "%04x:" "$block")"
+	done
+	# trim trailing ':'
+	blocks="${blocks_temp%?}"
+	printf "%s" "$blocks"
+)
+
+# returns a compressed ipv6 address under the form recommended by RFC5952
+# expects fully expanded ipv6 address as input, otherwise may produce incorrect results
+compress_ipv6 () (
+	addr="$1"
+	compress_var="$(printf "%s" "$addr" | sed -e 's/::/:0:/g' | grep -o "[0-9a-f]\+" | while read -r compress_var_hex; do [ -n "$compress_var_hex" ] && \
+		printf ":%x" "$((0x$compress_var_hex))"; done)"
+	for zero_chain in :0:0:0:0:0:0:0:0 :0:0:0:0:0:0:0 :0:0:0:0:0:0 :0:0:0:0:0 :0:0:0:0 :0:0:0 :0:0
+	do
+		case "$compress_var" in
+			*$zero_chain* )
+				compress_var="$(printf "%s" "$compress_var" | sed -e "s/$zero_chain/::/" -e 's/:::/::/')"
+				break
+		esac
+	done
+
+	case "$compress_var" in
+		::*) ;;
+		:*) compress_var="${compress_var#:}"
+	esac
+	printf "%s" "$compress_var"
+)
+
+# formats the input bytes as an ipv4 or ipv6 address
+format_ip() (
+	bytes="$1"
+	family="$2"
+
+	case "$family" in
+		inet )
+			# shellcheck disable=SC2086
+			printf "%d.%d.%d.%d\n" $bytes
+			return 0
+		;;
+		inet6 )
+			# shellcheck disable=SC2086
+			addr="$(printf "%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x\n" $bytes)"
+			addr_compressed="$(compress_ipv6 "$addr")"
+			validate_ip "$addr" "$family" || \
+				{ echo "format_ip(): Error: Failed to compress address '$addr', resulting ip '$addr_compressed' is invalid.'" >&2; return 1; }
+			printf "%s" "$addr_compressed"
+			return 0
+		;;
+		* ) echo "format_ip(): Error: invalid family '$family'" >&2; return 1
+	esac
+)
+
+# generates a mask represented as 16 1-byte hex chunks
+mask() (
+	maskbits="$1"
+
+	res=""
+	for i in $(seq 0 15); do
+		b=0
+		j=$(( maskbits - 8 * i))
+		if [ $j -ge 8 ]; then
+			b=255
+		elif [ $j -gt 0 ]; then
+			b=$(( (255 << (8-j)) & 255 ))
+		else
+			b=0
+		fi
+		res="$res $b"
+	done
+
+	# trim extra spaces
+	res="$(printf "%s" "$res" | awk '{$1=$1};1')"
+	printf "%s" "$res"
+)
+
+validate_ip () (
+# validates both ipv4 and ipv6 addresses, with or without mask bits
+# first performs regex validation, then runs the address through 'ip route get' command
+	addr="$1"
+	family="$2"
+
+	maskbits="$(printf "%s" "$addr" | awk -F/ '{print $2}')"
+	# chop off mask bits
+	addr="$(printf "%s" "$addr" | awk -F/ '{print $1}')"
+
+
+	case "$family" in
+		inet )
+			if [ -n "$maskbits" ]; then
+				printf "%s" "$maskbits" | grep -E "${maskbits_regex_ipv4}" > /dev/null || \
+					{ echo "validate_ip(): Error: failed to validate $family mask bits '$maskbits' with 'grep -E ${maskbits_regex_ipv4}'." >&2; return 1; }
+			fi
+			printf "%s" "$addr" | grep -E "${ipv4_regex}" > /dev/null || \
+				{ echo "validate_ip(): Error: failed to validate ipv4 address: '$addr' with 'grep -E ${ipv4_regex}'." >&2; return 1; }
+		;;
+
+		inet6 )
+			if [ -n "$maskbits" ]; then
+				printf "%s" "$maskbits" | grep -E "${maskbits_regex_ipv6}" > /dev/null || \
+					{ echo "validate_ip(): Error: failed to validate $family mask bits '$maskbits' with 'grep -E ${maskbits_regex_ipv6}'." >&2; return 1; }
+			fi
+			printf "%s" "$addr" | grep -E "${ipv6_regex}" > /dev/null || \
+				{ echo "validate_ip(): Error: failed to validate ipv6 address: '$addr' with 'grep -E ${ipv6_regex}'." >&2; return 1; }
+		;;
+		* ) echo "validate_ip(): Error: invalid family '$family'" >&2; return 1
+	esac
+
+	# using the 'ip route get' command to put the address through kernel's validation
+	# it normally returns 0 if the ip address is correct and it has a route, 1 if the address is invalid
+	# 2 if validation successful but for some reason it doesn't want to check the route ('permission denied')
+	ip route get "$addr" >/dev/null 2>/dev/null; rv=$?
+	[ $rv -eq 1 ] && { echo "validate_ip(): Error: failed to validate the ip '$addr' with command 'ip route get'." >&2; return 1; }
+	return 0
+)
+
+
+# Main
+cut_ip() (
+	# check dependencies
+
+	# test grep -E
+	rv=0
+	printf "%s" "32" | grep -E "${maskbits_regex_ipv4}" > /dev/null || rv=1
+	printf "%s" "0" | grep -E "${maskbits_regex_ipv4}" > /dev/null && rv=$((rv + 1))
+	[ "$rv" -ne 0 ] && { echo "cut_ip(): Error: 'grep -E' command is not working correctly on this machine." >&2; return 1; }
+	rv=0
+	ip route get "127.0.0.1" >/dev/null 2>/dev/null; rv_ipv4=$?
+	[ $rv_ipv4 -eq 1 ] && echo "cut_ip(): Warning: command 'ip addr get' is not working correctly on this machine for ipv4." >&2
+	ip route get "::1" >/dev/null 2>/dev/null; rv_ipv6=$?
+	[ $rv_ipv6 -eq 1 ] && echo "cut_ip(): Warning: command 'ip addr get' is not working correctly on this machine for ipv6." >&2
+
+	if [ ! "$rv_ipv4" ] && [ ! "$rv_ipv6" ]; then
+		echo "cut_ip(): Error: 'ip route get' command is not present or not working on this machine." >&2; return 1
+	fi
+
+
+	addr="$1"
+	family="$2"
+
+	[ "$family" = "inet" ] && [ ! "$rv_ipv4" ] && \
+		{ echo "cut_ip(): Can't process ipv4 addresses." >&2; return 1; }
+	[ "$family" = "inet6" ] && [ ! "$rv_ipv6" ] && \
+		{ echo "cut_ip(): Can't process ipv6 addresses." >&2; return 1; }
+	
+
+	# get mask bits
+	maskbits="$(printf "%s" "$addr" | awk -F/ '{print $2}')"
+
+	[ -z "$maskbits" ] && { echo "cut_ip(): Error: input '$addr' has no mask bits."; return 1; }
+	validate_ip "$addr" "$family" || { echo "cut_ip(): Error: ip '$addr' failed validation'"; return 1; }
+
+	# chop off mask bits
+	addr="$(printf "%s" "$addr" | awk -F/ '{print $1}')"
+
+	ip_bytes="$(ip_to_bytes "$addr" "$family")" || { echo "Error converting ip to bytes" >&2; return 1; }
+	mask_bytes="$(mask "$maskbits")"
+
+	[ "$family" = "inet6" ] && mask_len=128 || mask_len=32
+
+	# perform bit-wise AND on the address and the mask
+	bytes=""
+	for i in $(seq 1 $(( mask_len/8 )) ); do
+		mask_byte="$(printf "%s" "$mask_bytes" | cut -d' ' -f "$i")"
+		ip_byte="$(printf "%s" "$ip_bytes" | cut -d' ' -f "$i")"
+		b=$(( ip_byte & mask_byte ))
+		bytes="$bytes $b"
+	done
+
+	# trim extra spaces
+	bytes="$(printf "%s" "$bytes" | awk '{$1=$1};1')"
+
+	subnet="$(format_ip "$bytes" "$family")/$maskbits"
+
+	# shellcheck disable=SC2015
+	validate_ip "$subnet" "$family" && { printf "%s\n" "$subnet"; return 0; } || \
+		{ echo "cut_ip(): Error converting '$addr/$maskbits' to subnet. Resulting subnet '$subnet' is invalid."; return 1; }
+)
+
+### Constants
+# ipv4 regex and cidr regex taken from here and modified for ERE matching:
+# https://stackoverflow.com/questions/5284147/validating-ipv4-addresses-with-regexp
+# the longer ("alternative") ipv4 regex from the top suggestion performs about 40x faster on a slow CPU with ERE grep than the shorter one
+# ipv6 regex taken from the BanIP code and modified for ERE matching
+# https://github.com/openwrt/packages/blob/master/net/banip/files/banip-functions.sh
+ipv4_regex='^((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])\.){3}(25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])$'
+ipv6_regex='^([0-9a-fA-F]{0,4}:){1,7}[0-9a-fA-F]{0,4}:?$'
+maskbits_regex_ipv6='^(12[0-8]|((1[0-1]|[1-9])[0-9])|[8-9])$'
+maskbits_regex_ipv4='^(3[0-2]|([1-2][0-9])|[8-9])$'
+
+
+# to test functions from external sourcing script, export the $test_cut_ip variable in that script
+if [ -z "$test_cut_ip" ]; then
+	cut_ip "$1" "$2" || exit 1
+else return 0
+fi
