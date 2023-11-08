8a8c6951337ef313c1c61c865326b8f5b80a9f58
amos <anton.khazan@gmail.com>
2023-11-08 23:18:27 +0200

    get-subnet-tests.sh: conform to changed api in get-subnet.sh

--- get-subnet-tests.sh
+++ get-subnet-tests.sh
@@ -10,27 +10,28 @@ script_dir=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)
 # shellcheck disable=SC2015
 
 export source_get_subnet="true"
+# shellcheck disable=SC1091
 . "$script_dir/get-subnet.sh" || { echo "$me: Error: Can't source '$script_dir/get-subnet.sh'." >&2; exit 1; }
 
 test_exp_comp_ipv6(){
 
 tests=" \
-2001:4567:1212:00b2:0000:0000:0000:0000 2001:4567:1212:b2::
-2001:4567:1111:56ff:0000:0000:0000:0000 2001:4567:1111:56ff::
-2001:4567:1111:56ff:0000:0000:0000:0001 2001:4567:1111:56ff::1
-ff02:0000:0000:0000:0000:0000:0000:0001 ff02::1
-ff05:0000:0000:0000:0000:0000:0001:0003 ff05::1:3
-ff02:0000:0000:0000:0000:0001:ff00:0001 ff02::1:ff00:1
-ff02:0000:0000:0000:0000:0001:ffce:4ee3 ff02::1:ffce:4ee3
-fe80:0000:0000:0000:021b:2bff:fece:4ee3 fe80::21b:2bff:fece:4ee3
-fd0e:2146:5cf5:4560:0000:0000:0000:0001 fd0e:2146:5cf5:4560::1
-2001:0db8:0000:0000:0000:0000:0002:0001 2001:db8::2:1
-2001:0db8:0000:0001:0001:0001:0001:0001 2001:db8:0:1:1:1:1:1
-2001:0db8:0000:0000:0001:0000:0000:0001 2001:db8::1:0:0:1
-2001:0db8:0000:0000:0000:0000:0000:0001 2001:db8::1
-0000:0000:0000:0000:0000:0000:0000:0000 ::
-0000:0000:0000:0000:0000:0000:0000:0001 ::1
-0001:0000:0000:0000:0000:0000:0000:0000 1::
+20014567121200b20000000000000000 2001:4567:1212:b2::
+20014567111156ff0000000000000000 2001:4567:1111:56ff::
+20014567111156ff0000000000000001 2001:4567:1111:56ff::1
+ff020000000000000000000000000001 ff02::1
+ff050000000000000000000000010003 ff05::1:3
+ff0200000000000000000001ff000001 ff02::1:ff00:1
+ff0200000000000000000001ffce4ee3 ff02::1:ffce:4ee3
+fe80000000000000021b2bfffece4ee3 fe80::21b:2bff:fece:4ee3
+fd0e21465cf545600000000000000001 fd0e:2146:5cf5:4560::1
+20010db8000000000000000000020001 2001:db8::2:1
+20010db8000000010001000100010001 2001:db8:0:1:1:1:1:1
+20010db8000000000001000000000001 2001:db8::1:0:0:1
+20010db8000000000000000000000001 2001:db8::1
+00000000000000000000000000000000 ::
+00000000000000000000000000000001 ::1
+00010000000000000000000000000000 1::
 "
 
 	# remove extra spaces and tabs
@@ -48,6 +49,16 @@ fd0e:2146:5cf5:4560:0000:0000:0000:0001 fd0e:2146:5cf5:4560::1
 		if [ -n "$expanded_ip" ] && [ -n "$compressed_ip" ]; then
 			printf "%s" "."
 
+			# expand_ipv6 test
+			result="$(expand_ipv6 "$compressed_ip")"
+			if [ "$result" != "$expanded_ip" ]; then
+				echo "Error in expand_ipv6 with input '$compressed_ip'. Expected '$expanded_ip', got '$result'."
+				status_exp=1
+			fi
+
+			# # this tests the validate_ip() function
+			# validate_ip "$result" "inet6" || echo "Expanded ipv6 failed validation: '$result'." >&2
+
 			# compress_ipv6 test
 			result="$(compress_ipv6 "$expanded_ip")"
 			if [ "$result" != "$compressed_ip" ]; then
@@ -58,21 +69,10 @@ fd0e:2146:5cf5:4560:0000:0000:0000:0001 fd0e:2146:5cf5:4560::1
 			# this tests the validate_ip() function
 			validate_ip "$result" "inet6" || echo "Compressed ipv6 failed validation: '$result'." >&2
 
-
-			# expand_ipv6 test
-			result="$(expand_ipv6 "$compressed_ip")"
-			if [ "$result" != "$expanded_ip" ]; then
-				echo "Error in expand_ipv6 with input '$compressed_ip'. Expected '$expanded_ip', got '$result'."
-				status_exp=1
-			fi
-
-			# this tests the validate_ip() function
-			validate_ip "$result" "inet6" || echo "Expanded ipv6 failed validation: '$result'." >&2
-
-
 			tests_done=$((tests_done+1))
 		fi
 	done
+
 	return $((status_exp + status_comp))
 }
 status=$?
