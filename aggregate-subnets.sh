48c468df7f0bdf0ce0d944cd953b640e31a95b18
amos <anton.khazan@gmail.com>
2023-11-11 21:46:33 +0200

    aggregate-subnets.sh: - use the new script name and export variable name when calling trim-subnet.sh - check dependencies - check for errors when converting from hex to dec and when performing bitwise calculations - convert input to lower case - actually remove duplicates from input - send all input ip's to validation at once - validate mask bits - check for errors returned from ip_to_hex() - remove empty lines from input (if any) - use while..do loops instead of for..do loops - re-written subnets comparison logic for faster processing, not relying on trim_subnet() anymore

--- aggregate-subnets.sh
+++ aggregate-subnets.sh
@@ -6,7 +6,7 @@
 # Posix-compliant shell script which calculates an efficient configuration for subnets given as an input 
 # by trimming down each input subnet to its mask bits and removing subnets that are encapsulated inside other subnets on the list.
 # Designed for easier automated creation of firewall rules, but perhaps someone has a different application for this functionality.
-# Utilizes the get-subnet.sh script as a library.
+# Utilizes the trim-subnet.sh script as a library.
 
 
 # arg 1 is family (inet or inet6)
@@ -19,30 +19,57 @@
 export LC_ALL=C
 me=$(basename "$0")
 script_dir=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)
-export source_get_subnet="true"
+export source_trim_subnet="true"
 
-# source the get-subnet.sh script
+# convert to lower case and store in a variable
+family="$(printf "%s" "$1" | awk '{print tolower($0)}')"; shift
+
+# source the trim-subnet.sh script
 # shellcheck disable=SC1091
-. "$script_dir/get-subnet.sh" || { echo "$me: Error: Can't source '$script_dir/get-subnet.sh'." >&2; exit 1; }
+. "$script_dir/trim-subnet.sh" || { echo "$me: Error: Can't source '$script_dir/trim-subnet.sh'." >&2; exit 1; }
 
-# 1st arg is family
-family="$1"; shift
+# check dependencies
+! command -v awk >/dev/null || ! command -v sed >/dev/null || ! command -v tr >/dev/null || \
+! command -v grep >/dev/null || ! command -v ip >/dev/null || ! command -v cut >/dev/null && \
+	{ echo "get_subnet(): Error: missing dependencies, can not proceed" >&2; exit 1; }
+
+# test 'grep -E'
+rv=0; rv1=0; rv2=0
+printf "%s" "32" | grep -E "${maskbits_regex_ipv4}" > /dev/null; rv1=$?
+printf "%s" "0" | grep -E "${maskbits_regex_ipv4}" > /dev/null; rv2=$?
+rv=$((rv1 || ! rv2))
+[ "$rv" -ne 0 ] && { echo "get_subnet(): Error: 'grep -E' command is not working correctly on this machine." >&2; exit 1; }
+unset rv rv1 rv2
+
+test_ip_route_get
+
+
+#### Constants
+newline='
+'
+# chunk length in bits
+chunk_len=32
+
+# characters representing each chunk
+char_num=$((chunk_len / 4))
+
+
+#### Main
 
 [ -z "$family" ] && { echo "$me: Specify family (inet or inet6) in 1st argument." >&2; exit 1; }
 
 case "$family" in
-	inet ) mask_len=32; addr_regex="$ipv4_regex" ;;
-	inet6 ) mask_len=128; addr_regex="$ipv6_regex" ;;
+	inet ) ip_bytes=4; mask_len=32; addr_regex="$ipv4_regex" ;;
+	inet6 ) ip_bytes=16; mask_len=128; addr_regex="$ipv6_regex" ;;
 	* ) echo "$me: Invalid family '$family'. Specify family (inet or inet6) in 1st argument." >&2; exit 1
 esac
 
-# remove duplicates from input and store in a variable
-input_subnets="$(printf "%s" "$*" | sort -u)"
+# remove duplicates from input, convert to lower case
+input_subnets="$(printf "%s" "$*" | tr ' ' '\n' | sort -u | tr '\n' ' ' | awk '{print tolower($0)}')"
 
-for in_subnet in $input_subnets; do
-	# trim the subnet to its mask bits
-	subnet="$(get_subnet "$in_subnet")" || { echo "$me: Error processing input '$in_subnet'." >&2; exit 1; }
+validate_ip "$(printf "%s" "$input_subnets" | awk -F/ '{print $1}')" || exit 1
 
+for subnet in $input_subnets; do
 	# get mask bits
 	maskbits="$(printf "%s" "$subnet" | awk -F/ '{print $2}')"
 	[ -z "$maskbits" ] && { echo "$me: Error: input '$subnet' has no mask bits." >&2; exit 1; }
@@ -50,90 +77,133 @@ for in_subnet in $input_subnets; do
 	# chop off mask bits
 	input_addr="$(printf "%s" "$subnet" | awk -F/ '{print $1}')"
 
+	# validate mask bits
+	if [ "$maskbits" -lt 8 ] || [ "$maskbits" -gt $mask_len ]; then echo "$me: Error: invalid $family mask bits '$maskbits'." >&2; exit 1; fi
+
 	# convert ip address to hex. ip_to_hex() is in the sourced script
-	subnet_hex="$(ip_to_hex "$input_addr" "$family")"
+	subnet_hex="$(ip_to_hex "$input_addr" "$family")" || exit 1
 	# prepend mask bits
-	subnets_hex="$(printf "%s/%s\n%s\n" "${maskbits}" "${subnet_hex}" "$subnets_hex")"
+	subnets_hex="$(printf "%s/%s\n%s" "$maskbits" "$subnet_hex" "$subnets_hex")"
 done
 
-# remove duplicates, then sort by mask bits
-sorted_subnets_hex="$(printf "%s\n" "$subnets_hex" | sort -u | sort -n)"
-
-newline='
-'
-OLDIFS="$IFS"
-
-[ "$debug" ] && echo "sorted_subnets_hex: '$sorted_subnets_hex'"
+# remove duplicates, sort by mask bits, remove empty lines if any
+sorted_subnets_hex="$(printf "%s\n" "$subnets_hex" | sort -u | sort -n | awk -F_ '$1{print $1}')"
 
 while [ -n "$sorted_subnets_hex" ]; do
-	# grab the 1st subnet
-	subnet1_hex="$(printf "%s\n" "$sorted_subnets_hex" | head -n 1)"
-	if [ -n "$subnet1_hex" ]; then
-		# get mask bits
-		maskbits="$(printf "%s" "$subnet1_hex" | cut -d/ -f1)"
+	## trim the 1st (largest) subnet on the list to its mask bits
 
-		# chop off mask bits
-		ip1_chunks="$(printf "%s" "$subnet1_hex" | cut -d/ -f2)"
+	# get the subnet
+	subnet1="$(printf "%s" "$sorted_subnets_hex" | head -n 1)"
+	[ "$debug" ] && echo >&2
+	[ "$debug" ] && echo "processing subnet: $subnet1" >&2
 
-		# generate mask formatted as 4-bytes long hex chunks
-		mask_chunks="$(generate_mask "$maskbits")" || exit 1
+	# get mask bits
+	maskbits="$(printf "%s" "$subnet1" | awk -F/ '{print $1}')"
 
-		[ "$debug" ] && echo "mask_chunks: '$mask_chunks'"
+	# chop off mask bits
+	ip1="$(printf "%s" "$subnet1" | cut -d/ -f2)"
 
-		# remove current subnet from the initial list
-		sorted_subnets_hex="$(printf "%s\n" "$sorted_subnets_hex"  | tail -n +2)"
+	# generate mask
+	mask="$(generate_mask "$maskbits")" || exit 1
 
-		IFS="$newline"
-		# iterate over all remaining subnets
-		for subnet2_hex in $sorted_subnets_hex; do
-			IFS="$OLDIFS"
-			# chop off mask bits
-			ip2_chunks="$(printf "%s" "$subnet2_hex" | cut -d/ -f2)"
-			if [ -n "$ip2_chunks" ]; then
-				ip2_chunk=""; bytes_diff=0
-				for i in $(seq 1 $(( mask_len/32 )) ); do
-					# perform bitwise AND on the 2nd address and the mask of 1st address
-					mask_chunk="$(printf "%s" "$mask_chunks" | cut -d' ' -f "$i")"
-					ip2_chunk="$(printf "%s" "$ip2_chunks" | cut -d' ' -f "$i")"
-					[ "$debug" ] && echo "mask_chunk: '$mask_chunk', ip2_chunk: '$ip2_chunk'"
-					# bitwise AND on a chunk of subnet2 and corresponding chunk of mask from subnet1
-					ip2_chunk=$(printf "%08x" $(( 0x$ip2_chunk & 0x$mask_chunk )) )
+	## perform bitwise AND on the ip and the mask
 
-					# check for difference between current chunk in subnet1 and subnet2
-					ip1_chunk="$(printf "%s" "$ip1_chunks" | cut -d' ' -f "$i")"
-					[ "$debug" ] && echo "ip1_chunk: '$ip1_chunk'"
-					bytes_diff=$((0x$ip1_chunk - 0x$ip2_chunk))
-					# if there is any difference, no need to calculate further
-					if [ $bytes_diff -ne 0 ]; then
-						[ "$debug" ] && echo "difference found" >&2
-						break
-					fi
-				done
+	# split input into whitespace-separated chunks
+	ip_chunks="$(printf "%s" "$ip1" | sed 's/.\{'$char_num'\}/& /g;s/[ ]$//')"
+	mask_chunks="$(printf "%s" "$mask" | sed 's/.\{'$char_num'\}/& /g;s/[ ]$//')"
 
-				# if no differences found, subnet2 is encapsulated in subnet1 - remove subnet2 from the list
-				if [ $bytes_diff -eq 0 ]; then
-					[ "$debug" ] && echo "No difference found" >&2
-					sorted_subnets_hex="$(printf "%s\n" "$sorted_subnets_hex" | grep -vx "$subnet2_hex")"
-				fi
-			fi
-		done
-		IFS="$OLDIFS"
+	ip1=""; ip1_chunks=""; bits_processed=0
+	for i in $(seq 1 $(( mask_len / chunk_len )) ); do
+		mask_chunk="$(printf "%s" "$mask_chunks" | cut -d' ' -f "$i")"
+		ip_chunk="$(printf "%s" "$ip_chunks" | cut -d' ' -f "$i")"
+		ip_chunk="$(printf "%0${char_num}x" $(( 0x$ip_chunk & 0x$mask_chunk )) )" || \
+			{ echo "$me: Error: failed to calculate '0x$ip_chunk & 0x$mask_chunk'."; exit 1; }
+		ip1="$ip1$(printf "%s" "${ip_chunk}")"
+		ip1_chunks="$ip1_chunks $(printf "%s" "${ip_chunk}")"
+		bits_processed=$((bits_processed + chunk_len))
 
-		ip1_merged="$(printf "%s" "$ip1_chunks" | tr -d ' ')"
-		[ "$debug" ] && echo "ip1_merged: '$ip1_merged'" >&2
-		# format from bytes back to ip
-		subnet1="$(format_ip "$ip1_merged" "$family")" || exit 1
-		[ "$debug" ] && echo "subnet1: '$subnet1'" >&2
-		# add current subnet to resulting list
-		if validate_ip "$subnet1"; then
-			# append mask bits
-			subnet1="${subnet1}/$maskbits"
-			res_subnets="${subnet1}${newline}${res_subnets}"
-		else
-			exit 1
+		[ "$debug" ] && echo "ip1_chunks: '$ip1_chunks'" >&2
+
+		# if we processed $maskbits bits already, no need to calculate further
+		if [ "$bits_processed" -ge "$maskbits" ]; then
+			bytes_missing=$(( (mask_len - bits_processed) /8 ))
+			# repeat 00 for every missing byte
+			for b in $(seq 1 $bytes_missing); do ip1="$ip1$(printf "%s" '00')"; done
+			break
 		fi
+	done
+
+	# remove leading whitespace
+	ip1_chunks="${ip1_chunks# }"
+
+	# remove current subnet from the list
+	sorted_subnets_hex="$(printf "%s" "$sorted_subnets_hex" | tail -n +2)"
+	remaining_subnets_hex="$sorted_subnets_hex"
+
+	# iterate over all remaining subnets
+	while [ -n "$remaining_subnets_hex" ]; do
+		subnet2_hex=$(printf "%s" "$remaining_subnets_hex" | head -n 1)
+		[ "$debug" ] && echo "comparing to subnet: '$subnet2_hex'" >&2
+
+		if [ -n "$subnet2_hex" ]; then
+			# chop off mask bits
+			ip2="$(printf "%s" "$subnet2_hex" | cut -d/ -f2)"
+			# split into whitespace-separated chunks
+			ip2_chunks="$(printf "%s" "$ip2" | sed 's/.\{'$char_num'\}/& /g;s/[ ]$//')"
+			[ "$debug" ] && echo "ip2_chunks: '$ip2_chunks'" >&2
+
+			ip2_chunk=""; ip2_differs=""; bytes_diff=0; bits_processed=0
+
+			for i in $(seq 1 $(( mask_len / chunk_len )) ); do
+				ip1_chunk="$(printf "%s" "$ip1_chunks" | cut -d' ' -f "$i")"
+				[ "$debug" ] && echo "ip1_chunk: '$ip1_chunk'"
+
+				# perform bitwise AND on the 2nd address and the mask of 1st address
+				mask_chunk="$(printf "%s" "$mask_chunks" | cut -d' ' -f "$i")"
+				ip2_chunk="$(printf "%s" "$ip2_chunks" | cut -d' ' -f "$i")"
+				# bitwise AND on a chunk of subnet2 and corresponding chunk of mask from subnet1
+				ip2_chunk=$(printf "%0${char_num}x" $(( 0x$ip2_chunk & 0x$mask_chunk )) ) || \
+							{ echo "$me: Error: failed to calculate '0x$ip2_chunk & 0x$mask_chunk'."; exit 1; }
+				[ "$debug" ] && echo "mask_chunk: '$mask_chunk', ip2_chunk: '$ip2_chunk'"
+
+				# check for difference between current chunk in subnet1 and subnet2
+
+				bytes_diff=$((0x$ip1_chunk - 0x$ip2_chunk)) || \
+							{ echo "$me: Error: failed to calculate '0x$ip1_chunk - 0x$ip2_chunk'."; exit 1; }
+				# if there is any difference, no need to calculate further
+				if [ $bytes_diff -ne 0 ]; then
+					[ "$debug" ] && echo "difference found" >&2
+					ip2_differs=true
+				fi
+
+				bits_processed=$((bits_processed + chunk_len))
+
+				# if we processed $maskbits bits already, no need to calculate further
+				[ "$bits_processed" -ge "$maskbits" ] && break
+			done
+
+			# if no differences found, subnet2 is encapsulated in subnet1 - remove subnet2 from the list
+			if [ -z "$ip2_differs" ]; then
+				[ "$debug" ] && echo "No difference found" >&2
+				sorted_subnets_hex="$(printf "%s\n" "$sorted_subnets_hex" | grep -vx "$subnet2_hex")"
+				chunks_processed=0
+			fi
+		fi
+		remaining_subnets_hex="$(printf "%s" "$remaining_subnets_hex" | tail -n +2)"
+	done
+
+	# format from hex back to ip
+	ip1="$(format_ip "$ip1" "$family")" || exit 1
+	if validate_ip "$ip1"; then
+		# append mask bits
+		subnet1="$ip1/$maskbits"
+		# add current subnet to resulting list
+		res_subnets="${subnet1}${newline}${res_subnets}"
+	else
+		exit 1
 	fi
 done
+
 
 echo "Aggregated subnets:" >&2
 printf "%s\n" "$res_subnets"
