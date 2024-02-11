141719465d731297d658a61d25d6be66bb9e32f6
amos <anton.khazan@gmail.com>
2024-02-11 12:44:22 +0200

    fixed ipv6 subnets detection in some cases

--- detect-local-subnets.sh
+++ detect-local-subnets.sh
@@ -25,13 +25,13 @@ script_dir=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)
 args=''; debugmode=''
 for arg in "$@"; do
 	case "$arg" in
-		-s ) subnets_only="true" ;;
-		-d ) export debugmode="true" ;;
-		-f ) family_arg="check" ;;
-		* ) case "$family_arg" in check) family_arg="$arg" ;; *) args="$args $arg"; esac
+		-s ) subnets_only=1 ;;
+		-d ) export debugmode=1 ;;
+		-f ) families_arg=check ;;
+		* ) case "$families_arg" in check) families_arg="$arg"; esac
 	esac
 done
-[ "$family_arg" = "check" ] && { echo "Specify family with '-f'." >&2; exit 1; }
+[ "$families_arg" = "check" ] && { echo "Specify family with '-f'." >&2; exit 1; }
 
 set -- "$args"
 
@@ -50,7 +50,7 @@ get_local_subnets() {
 				ip -o -f inet addr show "$iface" | grep -oE "$subnet_regex_ipv4"
 			done
 		else
-			ip -o -f inet6 addr show | grep -oE "(fd[0-9a-f]{0,2}:|fe80:)(([[:alnum:]:/])+)" | grep -oE "^$subnet_regex_ipv6$"
+			ip -o -f inet6 addr show | grep -oE "inet6[[:space:]]+(fd[0-9a-f]{0,2}:|fe80:)(([[:alnum:]:/])+)" | grep -oE "^$subnet_regex_ipv6$"
 		fi
 	)"
 
@@ -78,12 +78,15 @@ get_local_subnets() {
 
 ## Main
 
-[ -n "$family_arg" ] && family_arg="$(printf '%s' "$family_arg" | tr 'A-Z' 'a-z')"
-case "$family_arg" in
-	inet|inet6 ) families="$family_arg" ;;
-	'' ) families="inet inet6" ;;
-	* ) printf '%s\n' "$me: Error: invalid family '$family_arg'." >&2; exit 1 ;;
-esac
+families=
+[ -n "$families_arg" ] && for word in $(printf '%s' "$families_arg" | tr 'A-Z' 'a-z'); do
+	case "$word" in
+		inet|ipv4) families="${families}inet " ;;
+		inet6|ipv6) families="${families}inet6 " ;;
+		*) printf '%s\n' "$me: Error: invalid family '$word'." >&2; exit 1
+	esac
+done
+: "${families:="inet inet6"}"
 
 rv=0
 for family in $families; do
